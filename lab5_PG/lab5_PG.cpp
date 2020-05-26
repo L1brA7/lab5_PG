1c84a33b3f0796887b24ecc12eba1bbc001d8721
LibrA <umashev58@gnail.com>
2020-05-26 19:59:18 +0300

    Menu

---
+++ lab5_PG/lab5_PG.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int number;
+	cout << "Enter the number, or enter 0 to close - ";
+	cin >> number;
+	while (number) {
+		if (number == 1) {}
+		if (number == 2) {}
+		if (number == 3) {}
+		cout << "Enter the number, or enter 0 to close - ";
+		cin >> number;
+	}
+}
